3ab789982c48ea3c2c653591530eb75384953903
keno80 <1211582428@qq.com>
2021-01-07 17:57:26 +0800

    添加歌单id操作

--- src/store/modules/musicList.js
+++ src/store/modules/musicList.js
@@ -1,6 +1,7 @@
 const state = {
 	detail: {},  //歌单详情、简介
 	songs: [],  //此歌单包含的歌曲
+	musicListIds: []  //歌单ID，用于判断当前歌单的歌曲是否存在与播放列表
 }
 
 const mutations = {
@@ -9,6 +10,12 @@ const mutations = {
 	},
 	SAVE_MUSIC_LIST_SONGS: (state, songs) => {
 		state.songs = songs
+	},
+	SAVE_MUSIC_LIST_IDS: (state, id) => {
+		state.musicListIds.push(id)
+	},
+	CLEAN_PLAY_LIST_STATE: (state) => {
+		state.musicListIds = []
 	}
 }
 
@@ -18,6 +25,12 @@ const actions = {
 	},
 	saveMusicListSongs({commit}, songs) {
 		commit('SAVE_MUSIC_LIST_SONGS', songs)
+	},
+	saveMusicListIds({commit}, id) {
+		commit('SAVE_MUSIC_LIST_IDS', id)
+	},
+	cleanPlayListState({commit}) {
+		commit('CLEAN_PLAY_LIST_STATE')
 	}
 }
 
