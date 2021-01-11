4da316478269daa4e6f8225b9af622e89bd84475
keno80 <1211582428@qq.com>
2021-01-11 18:02:34 +0800

    添加歌单api

--- src/views/Discover/api.js
+++ src/views/Discover/api.js
@@ -24,5 +24,54 @@ export default {
 			url: `${baseUrl}/personalized/newsong?limit=12`,
 			method: 'get'
 		})
+	},
+
+	//热门歌单分类标签列表
+	hotMusicListTag() {
+		return axios({
+			url: `${baseUrl}/playlist/hot`,
+			method: 'get'
+		})
+	},
+
+	//歌单分类标签
+	musicListTags() {
+		return axios({
+			url: `${baseUrl}/playlist/catlist`,
+			method: 'get'
+		})
+	},
+
+	// 精品歌单标签列表
+	HQMusicListTags() {
+		return axios({
+			url: `${baseUrl}/playlist/highquality/tags`,
+			method: 'get'
+		})
+	},
+
+	//歌单列表（网友精选碟）
+	musicList(cat, limit, order) {
+		return axios({
+			url: `${baseUrl}/top/playlist`,
+			method: 'get',
+			params: {
+				cat,
+				limit,
+				order
+			}
+		})
+	},
+
+	// 精品歌单列表
+	HQMusicList(cat, limit) {
+		return axios({
+			url: `${baseUrl}/top/playlist/highquality`,
+			method: 'get',
+			params: {
+				cat,
+				limit
+			}
+		})
 	}
 }
