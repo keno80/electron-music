bcf68a8573e8bd517c188a226ed0ca704a6baa95
keno80 <1211582428@qq.com>
2021-01-18 15:58:17 +0800

    完善官方榜简介布局

--- src/views/Discover/components/tab4-toplist/topListTable.vue
+++ src/views/Discover/components/tab4-toplist/topListTable.vue
@@ -1,18 +1,50 @@
 <template>
   <div class="top_list_tab">
-
+    <div class="top_list_tab_item" v-for="(item, index) in data" :class="[index%2 === 0 ? 'dark': '']">
+      <p class="first">{{ item.first }}</p>
+      <p class="second">{{ item.second }}</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "topListTable"
+  name: "topListTable",
+  props: {
+    data: Array
+  }
 }
 </script>
 
 <style scoped lang="scss">
 .top_list_tab {
-  margin-left: 40px;
+  margin: 0 20px 0 14px;
   width: 100%;
+
+  .top_list_tab_item {
+    border-radius: 4px;
+    padding: 6px;
+
+    &:hover {
+      background-color: #f3f3f3;
+    }
+
+    .first {
+      margin-bottom: 2px;
+    }
+
+    .second {
+      font-size: 12px;
+      color: #acacac;
+    }
+
+    p {
+      margin: 0;
+    }
+  }
+
+  .dark {
+    background-color: rgb(249, 249, 249);
+  }
 }
 </style>
