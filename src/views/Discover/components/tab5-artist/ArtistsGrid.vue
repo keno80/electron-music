7511ca025bbd07bc2addef5174a43d5a984bd79a
keno80 <1211582428@qq.com>
2021-01-19 15:46:08 +0800

    添加歌手列表页

---
+++ src/views/Discover/components/tab5-artist/ArtistsGrid.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="artists_list_grid">
+    <div class="artists_grid_item" v-for="(item, index) in list" :key="index">
+      <a><img :src="item.img1v1Url + '?param=139y139'"/></a>
+      <p><a>{{item.name}}</a></p>
+      <a>
+        <a-icon type="user" class="user_account" v-if="item.accountId"/>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ArtistsGrid",
+  props: {
+    list: Array
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.artists_list_grid {
+  margin-top: 10px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+
+  .artists_grid_item {
+    margin-bottom: 16px;
+    position: relative;
+
+    img {
+      border-radius: 6px;
+      border: 1px solid #f1f1f1;
+    }
+
+    p {
+      margin: 6px 0 0 0;
+
+      a {
+        color: #393939;
+      }
+    }
+
+    .user_account {
+      position: absolute;
+      top: 148px;
+      right: 2px;
+      background-color: #ec4141;
+      color: #ffffff;
+      padding: 2px;
+      border-radius: 50%;
+    }
+  }
+}
+</style>
